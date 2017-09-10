25c76bc6b3c1138081fa82dfdfe67ade4e6a0618
Heather Thacker <hhthacker@gmail.com>
2017-09-09 21:49:10 -0500

    files for adding product

---
+++ Bangazon/Bangazon/DataAccess/ProductDataAccess.cs
@@ -0,0 +1,37 @@
+﻿using Bangazon.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using System.Web.Http;
+
+namespace Bangazon.DataAccess
+{
+    public class ProductDataAccess
+    {
+        public List<ProductItem> GetAll()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+            {
+                connection.Open();
+
+                var result = connection.Query<ProductItem>("select * " +
+                                                            "from Products");
+                return result.ToList();
+            }
+        }
+
+        public int Post(ProductItem product)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+            {
+                var results = connection.Execute("Insert into Products(ProductName, InStock, Price, Description)" +
+                                                    "Values(@ProductName, @InStock, @Price, @Description)",
+                                                    new { ProductName = product.ProductName, InStock = product.InStock, Price = product.Price, Description = product.Description });
+                return results;
+            }
+        }
+    }
+}
---
+++ Bangazon/Bangazon/Models/ProductItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace Bangazon.Models
+{
+    public class ProductItem
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int InStock { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
