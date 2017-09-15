f78d581c5ce6db0f339c44c713c0c7b9b77b6aaf
bryanhmiller <bryankalos@hotmail.com>
2017-09-14 19:30:08 -0500

    Created OrderLineItemsListResults model

---
+++ Bangazon/Bangazon/Models/OrderLineItemsListResults.cs
@@ -0,0 +1,11 @@
+﻿namespace Bangazon.Models
+{
+    public class OrderLineItemsListResults
+    {
+        public int OrderLineID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
