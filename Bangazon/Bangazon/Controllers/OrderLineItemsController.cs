53c79f27fc55c263fd499abccf17443ede92a971
bryanhmiller <bryankalos@hotmail.com>
2017-09-14 20:05:15 -0500

    Created OrderLineItemsController

---
+++ Bangazon/Bangazon/Controllers/OrderLineItemsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using Bangazon.Models;
+using Bangazon.DataAccess;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bangazon.Controllers
+{
+    public class OrderLineItemsController : ApiController
+    {
+        //api/Order
+        public HttpResponseMessage Post(OrderListResult order)
+        {
+            using (var connection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["Bangazon"].ConnectionString))
+                try
+                {
+                    var addOrderLineItemsData = new OrderLineItemsDataAccess();
+                    addOrderLineItemsData.Add(order);
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Add order blew up", ex);
+                };
+        }
+    }
+}
+
