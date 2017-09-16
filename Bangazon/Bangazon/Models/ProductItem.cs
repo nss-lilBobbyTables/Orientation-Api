90901fe1e459b926a89529e9b5ed08df0aad7755
unknown <anessa.ortner@gmail.com>
2017-09-16 07:39:56 -0700

    setting product to out of stock method created

--- Bangazon/Bangazon/Models/ProductItem.cs
+++ Bangazon/Bangazon/Models/ProductItem.cs
@@ -10,7 +10,7 @@ namespace Bangazon.Models
     {
         public int ProductID { get; set; }
         public string ProductName { get; set; }
-        public int InStock { get; set; }
+        public int Quantity { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
 
